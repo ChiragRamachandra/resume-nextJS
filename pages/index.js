d989d3072c99d66684118319f1ab4f5c0c700c67
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-06-17 16:05:52 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<Navbar bg="light" expand="lg">
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Form inline>
+							<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+							<Button variant="outline-success">Search</Button>
+						</Form>
+					</Navbar.Collapse>
+				</Navbar>
+			</main>
+
+			<footer>
+				{/* <a
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+				</a> */}
+			</footer>
+		</div>
+	);
+}
